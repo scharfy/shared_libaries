6c2edfe66d81a9058d3f30bc66aa00e0410ba720
Niklas <nscharfy@gmail.com>
2021-06-18 11:26:29 +0200

    lol

--- vars/timedCommand2.groovy
+++ vars/timedCommand2.groovy
@@ -1,1 +1,21 @@
+def setCommand(commandToRun){
+    cmd = commandToRun
+}
+
+def getCommand(){
+    cmd
+}
+
+def runCommand(){
+    timestamps{
+        cmdOut = sh (script: "${cmd}", returnStdout:true).trim()
+        echo cmdOutput
+        writeFile
+    }
+}
+
+def getOutput(){
+    cmdOut
+}
+
 
