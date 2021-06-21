1d95865269ab5e9a97a491815d5808457a3de5a9
Niklas <nscharfy@gmail.com>
2021-06-21 08:40:48 +0200

    Feature

---
+++ vars/thirdparty/timedCommand3.groovy
@@ -0,0 +1,12 @@
+
+@Grab('org.apache.commons:commons-lang3:3.4+')
+import org.apache.commons.lang.time.StopWatch
+
+def call(String cmdToRun){
+    def sw = new StopWatch()
+    def proc = "$cmdToRun".execute()
+    sw.start()
+    proc.waitFor()
+    sw.stop()
+    println("The process took ${(sw.getTime()/1000).toString()} seconds. \n")
+}
